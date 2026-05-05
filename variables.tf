5193b2dbd48c2979a5cee76cfee8d22672f8aaf7
Naresh Gantala <nareshgantala@gmail.com>
2026-05-05 11:35:43 +0530

    variable fix

--- variables.tf
+++ variables.tf
@@ -8,17 +8,17 @@ variable "location" {
 
 variable "component" {
   default = {
-    frontend = Standard_B1s
-    mysql = Standard_B1s
-    catalogue = Standard_B1s
-    mongodb = Standard_B1s
-    user = Standard_B1s
-    valkey = Standard_B1s
-    cart = Standard_B1s
-    shipping = Standard_B1s
-    rabbitmq = Standard_B1s
-    payment = Standard_B1s
-    orders = Standard_B1s
-    ratings = Standard_B1s
+    frontend = "Standard_B1s"
+    mysql = "Standard_B1s"
+    catalogue = "Standard_B1s"
+    mongodb = "Standard_B1s"
+    user = "Standard_B1s"
+    valkey = "Standard_B1s"
+    cart = "Standard_B1s"
+    shipping = "Standard_B1s"
+    rabbitmq = "Standard_B1s"
+    payment = "Standard_B1s"
+    orders = "Standard_B1s"
+    ratings = "Standard_B1s"
   }
 }
