173296dd982a6ccf423f5f5850cccfbf01ba13a4
Naresh Gantala <nareshgantala@gmail.com>
2026-05-07 17:52:46 +0530

    updated image

--- modules/vm/data.tf
+++ modules/vm/data.tf
@@ -1,4 +1,6 @@
-data "azurerm_image" "img" {
-  name                = "roboshopImage"
+data "azurerm_shared_image_version" "img" {
+  name                = "1.0.0"
+  image_name          = "roboshopImage"
+  gallery_name        = "roboshopGallery"
   resource_group_name = var.resource_group_name
 }
