f32651dfb3298bf5247e234eca36c21acbfd5902
Naresh Gantala <nareshgantala@gmail.com>
2026-05-07 19:40:54 +0530

    update

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -16,3 +16,22 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_id = data.azurerm_shared_image_version.img.id
 
 }
+
+
+resource "null_resource" "name" {
+  connection {
+      type = "ssh"
+      user = "devops"
+      password = "DevOps@123456"
+      host = azurerm_linux_virtual_machine.main.private_ip_address
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    inline = [
+      "sudo dnf install python3-pip -y",
+      "sudo pip3.12 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/nareshgantala/roboshop-azure-ansible.git site.yml -e component_name=${each.key} -e env=dev",      
+    ]
+  }
+}
