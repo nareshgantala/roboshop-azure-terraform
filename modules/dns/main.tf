d19a68468a562abdab9c19ea022f716c39c68a3e
Naresh Gantala <nareshgantala@gmail.com>
2026-05-08 15:26:40 +0530

    update

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_dns_a_record" "main" {
   name                = "${var.component_name}-${var.env}"
-  zone_name           = "naresh-training.com"
+  zone_name           = "naresh-training.online"
   resource_group_name = var.resource_group_name
   ttl                 = 300
   records             = [ var.record ]
