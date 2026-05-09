87ff7bcc7c82ef3db7a8e870f9446f2c0b48de78
Naresh Gantala <nareshgantala@gmail.com>
2026-05-09 13:59:50 +0530

    update

--- provider.tf
+++ provider.tf
@@ -6,9 +6,6 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
   # Can also be set via `ARM_CLIENT_ID` environment variable.
-    storage_account_name = "roboshop"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstates"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"  
-    resource_group_name  =  "denmark-east"         # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
