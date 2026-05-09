77ec1d5b335e061fc24e38ea406e5fbdbac0c6f4
Naresh Gantala <nareshgantala@gmail.com>
2026-05-09 12:14:08 +0530

    update

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
   # Can also be set via `ARM_CLIENT_ID` environment variable.
     storage_account_name = "roboshop"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstates"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "terraform.tfstate"  
+    resource_group_name  =  "denmark-east"         # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
