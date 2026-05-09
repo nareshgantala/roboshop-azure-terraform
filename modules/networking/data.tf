4cc6a6ad0feb283575227d7d86527ca0464dcdf9
Naresh Gantala <nareshgantala@gmail.com>
2026-05-09 12:05:04 +0530

    update network with nat

--- modules/networking/data.tf
+++ modules/networking/data.tf
@@ -2,3 +2,9 @@ data "azurerm_network_security_group" "nsg" {
   name                = "allow-all"
   resource_group_name = var.resource_group_name
 }
+
+data "azurerm_subnet" "default_subnet" {
+  name                 = "default"
+  virtual_network_name = "test-virtual-network"
+  resource_group_name  = var.resource_group_name
+}
