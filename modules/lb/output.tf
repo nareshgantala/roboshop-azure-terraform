8162026156df9dea0f642037f3c4c2c9987d496a
Naresh Gantala <nareshgantala@gmail.com>
2026-05-15 14:19:33 +0530

    up

--- modules/lb/output.tf
+++ modules/lb/output.tf
@@ -1,16 +1,16 @@
 output "ui_pool_id" {
-  value = azurerm_lb_backend_address_pool.ui_pool.id
+  value = azurerm_lb_backend_address_pool.ui_pool[count.index].id
 }
 
 output "app_pool_id" {
-  value = azurerm_lb_backend_address_pool.app_pool.id
+  value = azurerm_lb_backend_address_pool.app_pool[count.index].id
 }
 
 output "lb_public_ip" {
-  value = azurerm_public_ip.lb_pip.ip_address
+  value = azurerm_public_ip.lb_pip[count.index].ip_address
 
 }
 
 output "app_lb_privateIP" {
-  value = azurerm_lb.main_app.private_ip_address
+  value = azurerm_lb.main_app[count.index].private_ip_address
 }
