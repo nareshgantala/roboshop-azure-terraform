e9113928bde2c5eb3f9da29afa71467a0dd6fbad
Naresh Gantala <nareshgantala@gmail.com>
2026-06-02 09:54:00 +0530

    network profile updated

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -15,6 +15,18 @@ resource "azurerm_kubernetes_cluster" "main" {
     type = "SystemAssigned"
   }
 
+  # ADD THIS BLOCK TO SOLVE THE OVERLAP
+  network_profile {
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+    
+    # Internal cluster services IP range (must not overlap with 10.0.0.0/16)
+    service_cidr       = "172.16.0.0/16"
+    
+    # Internal DNS server address (must be an IP inside the service_cidr range)
+    dns_service_ip     = "172.16.0.10"
+  }
+
   tags = {
     Environment = var.env
   }
