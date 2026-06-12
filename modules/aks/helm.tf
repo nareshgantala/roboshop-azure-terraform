b879a4a56a54d9ad2c35cd48c19b647c20bd9264
Naresh Gantala <nareshgantala@gmail.com>
2026-06-12 17:13:14 +0530

    prom added

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -8,7 +8,15 @@ resource "null_resource" "kube-config" {
 
 
 resource "helm_release" "traefik_ingress" {
+  depends_on = [ null_resource.kube-config ]
   name       = "traefik"
   repository = "https://traefik.github.io/charts"
   chart      = "traefik"
+}
+
+resource "helm_release" "prom_stack" {
+  depends_on = [ null_resource.kube-config, helm_release.traefik_ingress ]
+  name       = "pstack"
+  repository = "oci://ghcr.io/prometheus-community/charts"
+  chart      = "kube-prometheus-stack"
 }
