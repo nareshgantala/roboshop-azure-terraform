e84e5649e25ba2a4810220bf32857dd9d5068cd4
Naresh Gantala <nareshgantala@gmail.com>
2026-06-13 13:23:43 +0530

    updated data file

--- modules/aks/data.tf
+++ modules/aks/data.tf
@@ -1,19 +1,19 @@
 data "azurerm_key_vault_secret" "ClientID" {
   name         = "externalDnsClientID"
-  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+  key_vault_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.KeyVault/vaults/roboshop"
 }
 
 data "azurerm_key_vault_secret" "ClientPassword" {
   name         = "ExternalDnsClientPassword"
-  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+  key_vault_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.KeyVault/vaults/roboshop"
 }
 
 data "azurerm_key_vault_secret" "PrometheusClientID" {
   name         = "PrometheusClientID"
-  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+  key_vault_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.KeyVault/vaults/roboshop"
 }
 
 data "azurerm_key_vault_secret" "PrometheusClientPassword" {
   name         = "PrometheusClientPassword"
-  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+  key_vault_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.KeyVault/vaults/roboshop"
 }
