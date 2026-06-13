fba1d13db8138ed4c532d19e230bc20b366fd2b9
Naresh Gantala <nareshgantala@gmail.com>
2026-06-13 13:26:28 +0530

    grafana pass

--- output.tf
+++ output.tf
@@ -6,3 +6,14 @@ output "location" {
   value = data.azurerm_resource_group.rsg.location
 }
 
+
+data "external" "grafana_secret" {
+  depends_on = [module.aks]
+  program    = ["bash", "${path.root}/k8s-secrets.sh"]
+}
+
+output "grafana_admin_password" {
+  # base64decode() takes care of the decoding inside Terraform
+  value = base64decode(data.external.grafana_secret.result["password"])
+}
+
